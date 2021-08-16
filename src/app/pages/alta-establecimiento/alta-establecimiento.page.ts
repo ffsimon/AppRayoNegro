584b7de83957eb4473c3f8442907d0916c2824be
Froylan <froylansimondomingo@gmail.com>
2021-08-16 00:09:57 -0500

    Modificacion en alta establecimiento

--- src/app/pages/alta-establecimiento/alta-establecimiento.page.ts
+++ src/app/pages/alta-establecimiento/alta-establecimiento.page.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonSlides, NavController } from '@ionic/angular';
@@ -15,18 +16,18 @@ export class AltaEstablecimientoPage implements OnInit {
 
   @ViewChild('slideWithNav', { static: false }) slideWithNav: IonSlides;
   public tempImg: string;
-  public fotoIdentificastePublicidad: string = '';
+  public fotoIdentificastePublicidad = '';
   public comentarios = null;
-  public compartieronRazonSocial: boolean = false;
-  public estasEnOutlet: boolean = false;
-  public pasoFormulario: number = 1;
+  public compartieronRazonSocial = false;
+  public estasEnOutlet = false;
+  public pasoFormulario = 1;
   public opcionesParafoto: any = [];
   public sliderOne: any;
   public listaMastercardRadio: any[] = [
     { nombre: 'Sticker', valor: 'sticker' },
     { nombre: 'Reloj (open / close)', valor: 'reloj' },
     { nombre: 'Acrilico', valor: 'acrilico' }
-  ]
+  ];
   public datosGenerales: FormGroup = this.formBuilder.group({
     nombreEstablecimiento: [null, Validators.required],
     razonSocial: [null, this.compartieronRazonSocial? [Validators.required]: []],
@@ -45,39 +46,39 @@ export class AltaEstablecimientoPage implements OnInit {
   });
   public listaTipoComercio: any = [];
   public subListaTipoComercio: any = [];
-  public subListaTipoComunicacion: any = [];  
+  public subListaTipoComunicacion: any = [];
   public subListaTipoLocalizacion: any = [];
   public lstaCompetencia: any = [];
   public listaCompetenciaCaracteristicas: any [];
-  public primerOpcionPasoCinco: boolean = false;
-  public segundaOpcionPasoCinco: boolean = false;
-  public tercerOpcionPasoCinco: boolean = false;
+  public primerOpcionPasoCinco = false;
+  public segundaOpcionPasoCinco = false;
+  public tercerOpcionPasoCinco = false;
 
   get nombreEstablecimiento() {
-    return this.datosGenerales.get("nombreEstablecimiento");
+    return this.datosGenerales.get('nombreEstablecimiento');
   }
   get razonSocial() {
-    return this.datosGenerales.get("razonSocial");
+    return this.datosGenerales.get('razonSocial');
   }
   get tipoComercio() {
-    return this.datosGenerales.get("tipoComercio");
+    return this.datosGenerales.get('tipoComercio');
   }
   get subTipoComercio() {
-    return this.datosGenerales.get("subTipoComercio");
+    return this.datosGenerales.get('subTipoComercio');
   }
   get nombreOutlet() {
-    return this.datosGenerales.get("nombreOutlet");
+    return this.datosGenerales.get('nombreOutlet');
   }
 
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   constructor(
-    private formBuilder: FormBuilder, 
-    private navCtrl: NavController, 
+    private formBuilder: FormBuilder,
+    private navCtrl: NavController,
     private utilitiesService: UtilitiesService,
     private webRayoService: WebRayoService,
     private camera: Camera) {
-      console.log(this.opcionesParafoto.length)
+      console.log(this.opcionesParafoto.length);
     this.sliderOne =
     {
       initialSlide: 0,
@@ -93,25 +94,25 @@ export class AltaEstablecimientoPage implements OnInit {
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   async ngOnInit() {
-    let loading = await this.utilitiesService.loadingAsync();
+    const loading = await this.utilitiesService.loadingAsync();
     loading.present();
 
     await this.obtenerTipoComercion();
     await this.obtenerListaComunicacion();
     await this.obtenerListaLocalizacion();
     await this.obtenerBaners();
-    await this.obtenerCompetencia(); 
+    await this.obtenerCompetencia();
     loading.dismiss();
   }
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async obtenerTipoComercion(){
-    let params = this.webRayoService.fromObjectToGETString({
+    const params = this.webRayoService.fromObjectToGETString({
       cagenerico_ca_tipo_id: 1
     });
-    let url = "Catalogos/CatalogoGenerico/Get" + params;
-    let respuesta: any = await this.webRayoService.getAsync(url);
-    if ( respuesta == null || respuesta.success == false ||respuesta.response.length == 0 ) {
+    const url = 'Catalogos/CatalogoGenerico/Get' + params;
+    const respuesta: any = await this.webRayoService.getAsync(url);
+    if ( respuesta == null || respuesta.success === false ||respuesta.response.length === 0 ) {
       this.listaTipoComercio = [];
     } else {
       this.listaTipoComercio = respuesta.response;
@@ -120,58 +121,57 @@ export class AltaEstablecimientoPage implements OnInit {
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public validacionDatosGenerales() {
 
-    console.log(this.pasoFormulario)
+    console.log(this.pasoFormulario);
 
-    if(this.pasoFormulario == 1){
-      console.log(this.datosGenerales.valid)
-      console.log(this.datosGenerales, this.compartieronRazonSocial, this.estasEnOutlet)
-      
+    if(this.pasoFormulario === 1){
+      console.log(this.datosGenerales.valid);
+      console.log(this.datosGenerales, this.compartieronRazonSocial, this.estasEnOutlet);
+
       if(!this.datosGenerales.valid){
-        this.utilitiesService.alert("", "Verifica que los datos esten completos.")
+        this.utilitiesService.alert('', 'Verifica que los datos esten completos.');
           return;
       }
 
-      if(this.compartieronRazonSocial && this.datosGenerales.value.razonSocial == null || 
-        this.compartieronRazonSocial && this.datosGenerales.value.razonSocial == ''){
-          this.utilitiesService.alert("", "Agrega la razón social.")
+      if(this.compartieronRazonSocial && this.datosGenerales.value.razonSocial == null ||
+        this.compartieronRazonSocial && this.datosGenerales.value.razonSocial === ''){
+          this.utilitiesService.alert('', 'Agrega la razón social.');
           return;
       }
 
-      if(this.estasEnOutlet && this.datosGenerales.value.nombreOutlet == null || 
-        this.estasEnOutlet && this.datosGenerales.value.nombreOutlet == ''){
-          this.utilitiesService.alert("", "Agrega nombre del outlet.")
-          return;
-      }
-      
-    }
-
-    if(this.pasoFormulario == 2){
-      if(this.datosUbicacion.value.numeroEstablecimiento == null || this.datosUbicacion.value.numeroEstablecimiento == ''){
-        this.utilitiesService.alert("", "Agrega el número del establecimiento.")
+      if(this.estasEnOutlet && this.datosGenerales.value.nombreOutlet == null ||
+        this.estasEnOutlet && this.datosGenerales.value.nombreOutlet === ''){
+          this.utilitiesService.alert('', 'Agrega nombre del outlet.');
           return;
       }
     }
-    
-    if(this.pasoFormulario == 3){
+
+    if(this.pasoFormulario === 2){
+      if(this.datosUbicacion.value.numeroEstablecimiento == null || this.datosUbicacion.value.numeroEstablecimiento === ''){
+        this.utilitiesService.alert('', 'Agrega el número del establecimiento.');
+          return;
+      }
+    }
+
+    if(this.pasoFormulario === 3){
       if(!this.seleccioneMaterial.valid){
-        this.utilitiesService.alert("", "Agregue todos los campos requeridos.")
-          return;
+        this.utilitiesService.alert('', 'Agregue todos los campos requeridos.');
+        return;
       }
 
-      console.log(this.sliderOne.slidesItems)
-      this.opcionesParafoto = this.sliderOne.slidesItems.filter(item => item.seleccionado == true)
-      console.log(this.opcionesParafoto)
+      console.log(this.sliderOne.slidesItems);
+      this.opcionesParafoto = this.sliderOne.slidesItems.filter(item => item.seleccionado === true);
+      console.log(this.opcionesParafoto);
     }
 
     let bandera = false;
-    if (this.pasoFormulario == 4) {
+    if (this.pasoFormulario === 4) {
       if (this.opcionesParafoto.length === 0) {
         if (this.tempImg === '' || this.tempImg === null) {
           bandera = true;
         }
       } else {
         this.opcionesParafoto.forEach(element => {
-          console.log(element.fotoBase64, this.tempImg)
+          console.log(element.fotoBase64, this.tempImg);
           if (element.fotoBase64 === '' || this.tempImg === '') {
             bandera = true;
           }
@@ -179,50 +179,48 @@ export class AltaEstablecimientoPage implements OnInit {
       }
 
       if (bandera) {
-        this.utilitiesService.alert("", "Todas las fotos son requeridas.");
+        this.utilitiesService.alert('', 'Todas las fotos son requeridas.');
         return;
       }
     }
 
-    if(this.pasoFormulario == 5){
-      if(this.tercerOpcionPasoCinco && this.fotoIdentificastePublicidad == ''){
-        console.log("falta foto");
-        this.utilitiesService.alert("", "Captura foto de publicidad nueva.");
+    if(this.pasoFormulario === 5){
+      if(this.tercerOpcionPasoCinco && this.fotoIdentificastePublicidad === ''){
+        console.log('falta foto');
+        this.utilitiesService.alert('', 'Captura foto de publicidad nueva.');
         return;
       }
     }
-
-    
 
     this.pasoFormulario = this.pasoFormulario + 1;
-    if (this.pasoFormulario == 6) {
-      this.utilitiesService.alert("¡Éxito!", "Se ha guardado exitosamente el establecimiento.")
-      this.navCtrl.navigateRoot("home");
+    if (this.pasoFormulario === 6) {
+      this.utilitiesService.alert('¡Éxito!', 'Se ha guardado exitosamente el establecimiento.');
+      this.navCtrl.navigateRoot('home');
     }
   }
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async obtenerListaComunicacion(){
-    let params = this.webRayoService.fromObjectToGETString({
+    const params = this.webRayoService.fromObjectToGETString({
       cagenerico_ca_tipo_id: 4
     });
-    let url = "Catalogos/CatalogoGenerico/Get" + params;
-    let respuesta: any = await this.webRayoService.getAsync(url);
-    if ( respuesta == null || respuesta.success == false ||respuesta.response.length == 0 ) {
+    const url = 'Catalogos/CatalogoGenerico/Get' + params;
+    const respuesta: any = await this.webRayoService.getAsync(url);
+    if ( respuesta == null || respuesta.success === false ||respuesta.response.length === 0 ) {
       this.subListaTipoComunicacion = [];
     } else {
       this.subListaTipoComunicacion = respuesta.response;
     }
   }
-  
+
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async obtenerListaLocalizacion(){
-    let params = this.webRayoService.fromObjectToGETString({
+    const params = this.webRayoService.fromObjectToGETString({
       cagenerico_ca_tipo_id: 5
     });
-    let url = "Catalogos/CatalogoGenerico/Get" + params;
-    let respuesta: any = await this.webRayoService.getAsync(url);
-    if ( respuesta == null || respuesta.success == false ||respuesta.response.length == 0 ) {
+    const url = 'Catalogos/CatalogoGenerico/Get' + params;
+    const respuesta: any = await this.webRayoService.getAsync(url);
+    if ( respuesta == null || respuesta.success === false ||respuesta.response.length === 0 ) {
       this.subListaTipoLocalizacion = [];
     } else {
       this.subListaTipoLocalizacion = respuesta.response;
@@ -231,12 +229,12 @@ export class AltaEstablecimientoPage implements OnInit {
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async obtenerBaners(){
-    let params = this.webRayoService.fromObjectToGETString({
+    const params = this.webRayoService.fromObjectToGETString({
       cagenerico_ca_tipo_id: 2
     });
-    let url = "Catalogos/CatalogoGenerico/Get" + params;
-    let respuesta: any = await this.webRayoService.getAsync(url);
-    if ( respuesta == null || respuesta.success == false ||respuesta.response.length == 0 ) {
+    const url = 'Catalogos/CatalogoGenerico/Get' + params;
+    const respuesta: any = await this.webRayoService.getAsync(url);
+    if ( respuesta == null || respuesta.success === false ||respuesta.response.length === 0 ) {
       this.sliderOne.slidesItems = [];
     } else {
       this.sliderOne.slidesItems = respuesta.response;
@@ -249,17 +247,17 @@ export class AltaEstablecimientoPage implements OnInit {
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async obtenerCompetencia(){
-    let params = this.webRayoService.fromObjectToGETString({
+    const params = this.webRayoService.fromObjectToGETString({
       cagenerico_ca_tipo_id: 6
     });
-    let url = "Catalogos/CatalogoGenerico/Get" + params;
-    let respuesta: any = await this.webRayoService.getAsync(url);
-    if ( respuesta == null || respuesta.success == false ||respuesta.response.length == 0 ) {
+    const url = 'Catalogos/CatalogoGenerico/Get' + params;
+    const respuesta: any = await this.webRayoService.getAsync(url);
+    if ( respuesta == null || respuesta.success === false ||respuesta.response.length === 0 ) {
       this.lstaCompetencia = [];
     } else {
       this.lstaCompetencia = respuesta.response;
       for (let i = 0; i < this.lstaCompetencia.length; i++) {
-       let hijoCompetencia =  await this.obtenerCompetenciaCaracteristica(this.lstaCompetencia[i].cagenerico_clave);  
+       let hijoCompetencia =  await this.obtenerCompetenciaCaracteristica(this.lstaCompetencia[i].cagenerico_clave);
        this.lstaCompetencia[i].arregloHijos = hijoCompetencia;
       }
     }
@@ -269,34 +267,32 @@ export class AltaEstablecimientoPage implements OnInit {
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async obtenerCompetenciaCaracteristica(idPadre:number){
-    let params = this.webRayoService.fromObjectToGETString({
+    const params = this.webRayoService.fromObjectToGETString({
       crelacion_id_padre: idPadre
     });
-    let url = "Catalogos/CatalogoArbol/Get" + params;
-    let respuesta: any = await this.webRayoService.getAsync(url);
-    if ( respuesta == null || respuesta.success == false ||respuesta.response.length == 0 ) {
+    const url = 'Catalogos/CatalogoArbol/Get' + params;
+    const respuesta: any = await this.webRayoService.getAsync(url);
+    if ( respuesta == null || respuesta.success === false ||respuesta.response.length === 0 ) {
      return [];
     } else {
-
       for (let i = 0; i < respuesta.response.length; i++) {
         respuesta.response[i].estatus = false;
-        
       }
-      console.log(respuesta.response)
-      return respuesta.response; 
+      console.log(respuesta.response);
+      return respuesta.response;
     }
   }
 
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public cerrarFormularios() {
-    this.navCtrl.navigateRoot("home");
+    this.navCtrl.navigateRoot('home');
   }
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async camara(): Promise<string> {
-    console.log("tomar foto")
-    let fotoBase64: string = ''
+    console.log('tomar foto');
+    let fotoBase64 = '';
     const options: CameraOptions = {
       quality: 50,
       destinationType: this.camera.DestinationType.DATA_URL,
@@ -307,11 +303,11 @@ export class AltaEstablecimientoPage implements OnInit {
     };
 
     await this.camera.getPicture(options).then((imageData) => {
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      const base64Image = 'data:image/jpeg;base64,' + imageData;
       fotoBase64 = base64Image;
     }, (err) => {
-      fotoBase64 = ''
-      console.log("error", err)
+      fotoBase64 = '';
+      console.log('error', err);
     });
 
     return fotoBase64;
@@ -319,22 +315,21 @@ export class AltaEstablecimientoPage implements OnInit {
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async cambiarValores(){
-    let valor = this.datosGenerales.value.tipoComercio.cagenerico_clave;
+    const valor = this.datosGenerales.value.tipoComercio.cagenerico_clave;
     this.subListaTipoComercio = await this.buscarSubtipoComercio(valor);
-    
   }
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async buscarSubtipoComercio(idPadre){
     this.subTipoComercio.setValue(null);
-    let loading = await this.utilitiesService.loadingAsync();
+    const loading = await this.utilitiesService.loadingAsync();
     loading.present();
 
-    let params = this.webRayoService.fromObjectToGETString({
+    const params = this.webRayoService.fromObjectToGETString({
       crelacion_id_padre: idPadre
     });
-    let url = "Catalogos/CatalogoArbol/Get" + params;
-    let respuesta: any = await this.webRayoService.getAsync(url);
-    if ( respuesta == null || respuesta.success == false ||respuesta.response.length == 0 ) {
+    const url = 'Catalogos/CatalogoArbol/Get' + params;
+    const respuesta: any = await this.webRayoService.getAsync(url);
+    if ( respuesta == null || respuesta.success === false ||respuesta.response.length === 0 ) {
       loading.dismiss();
       return this.subListaTipoComercio = [];
     } else {
@@ -345,32 +340,31 @@ export class AltaEstablecimientoPage implements OnInit {
   }
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  public limpiarInput(tipo:number){
+  public limpiarInput(tipo: number){
     //1 para limpiar input de razon social, 2 para limpiar outlet
-    if(tipo == 1){
+    if (tipo === 1) {
       this.razonSocial.setValue(null);
       return;
-    }else{
+    } else {
       this.nombreOutlet.setValue(null);
-    } 
+    }
   }
-  
+
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public ayuda(){
-    this.utilitiesService.toast("Ayuda");
+    this.utilitiesService.toast('Ayuda');
   }
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async tomarFoto(opt: number, posicion?: number) {
-    if (opt == 1){
+    if (opt === 1) {
       this.tempImg = await this.camara();
       console.log(this.tempImg);
-    } else if (opt == 2) {
+    } else if (opt === 2) {
       this.opcionesParafoto[posicion].fotoBase64 = await this.camara();
       console.log(this.opcionesParafoto);
-    }else if(opt == 3){
+    } else if (opt === 3){
       this.fotoIdentificastePublicidad = await this.camara();
     }
   }
-
 }
--- src/app/pages/evaluaciones/evaluaciones.page.ts
+++ src/app/pages/evaluaciones/evaluaciones.page.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { usuario_sesion_model } from 'src/app/models/usuario_sesion';
@@ -11,35 +12,35 @@ import { WebRayoService } from 'src/app/services/web-rayo.service';
 })
 export class EvaluacionesPage implements OnInit {
 
-  public mostrarDetalle: boolean = false;
+  public mostrarDetalle = false;
   public listaEvaluacionesUsuario: any = [];
-  public nombreTipoComercio: string = '';
+  public nombreTipoComercio = '';
   public listaTipoComercio: any = [];
-  public usuarioSesion: usuario_sesion_model; 
+  public usuarioSesion: usuario_sesion_model;
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   constructor(private navCtrl: NavController,
-     private webRayoService: WebRayoService, 
-     private utilitiesService: UtilitiesService) {
-    this.usuarioSesion = JSON.parse(sessionStorage.getItem("usuario_sesion"));
-    console.log(this.usuarioSesion)
-   }
+    private webRayoService: WebRayoService,
+    private utilitiesService: UtilitiesService) {
+    this.usuarioSesion = JSON.parse(sessionStorage.getItem('usuario_sesion'));
+    console.log(this.usuarioSesion);
+  }
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async ngOnInit() {
-    let loading = await this.utilitiesService.loadingAsync();
+    const loading = await this.utilitiesService.loadingAsync();
     loading.present();
     await this.obtenerEvaluacionesUsuario();
-    loading.dismiss()
+    loading.dismiss();
   }
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async obtenerEvaluacionesUsuario(){
-    let params = this.webRayoService.fromObjectToGETString({
+    const params = this.webRayoService.fromObjectToGETString({
       evaluacion_tbl_usuarios_id: this.usuarioSesion.user_id
     });
-    let url = "Operaciones/Evaluacion/Get" + params;
-    let respuesta: any = await this.webRayoService.getAsync(url);
-    if ( respuesta == null || respuesta.success == false ||respuesta.response.length == 0 ) {
+    const url = 'Operaciones/Evaluacion/Get' + params;
+    const respuesta: any = await this.webRayoService.getAsync(url);
+    if ( respuesta === null || respuesta.success === false ||respuesta.response.length === 0 ) {
       this.listaEvaluacionesUsuario = [];
     } else {
       this.listaEvaluacionesUsuario = respuesta.response;
@@ -48,12 +49,12 @@ export class EvaluacionesPage implements OnInit {
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public async obtenerTipoComercion(){
-    let params = this.webRayoService.fromObjectToGETString({
+    const params = this.webRayoService.fromObjectToGETString({
       cagenerico_ca_tipo_id: 1
     });
-    let url = "Catalogos/CatalogoGenerico/Get" + params;
-    let respuesta: any = await this.webRayoService.getAsync(url);
-    if ( respuesta == null || respuesta.success == false ||respuesta.response.length == 0 ) {
+    const url = 'Catalogos/CatalogoGenerico/Get' + params;
+    const respuesta: any = await this.webRayoService.getAsync(url);
+    if ( respuesta === null || respuesta.success === false ||respuesta.response.length === 0 ) {
       this.listaTipoComercio = [];
     } else {
       this.listaTipoComercio = respuesta.response;
@@ -62,12 +63,16 @@ export class EvaluacionesPage implements OnInit {
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public regresarHome() {
-    this.navCtrl.navigateRoot("home");
+    this.navCtrl.navigateRoot('home');
   }
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   public irEstablecimiento() {
-    this.navCtrl.navigateRoot("alta-establecimiento");
+    this.navCtrl.navigateRoot('alta-establecimiento');
+  }
+
+  //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  public irEvaluaciones(){
   }
 
 
