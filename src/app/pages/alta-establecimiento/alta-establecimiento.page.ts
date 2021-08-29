cc25c8655f739aa6628195eb838da595ab6db710
alengalvan <alengalvan95@gmail.com>
2021-08-28 21:31:33 -0500

    bug

--- src/app/pages/alta-establecimiento/alta-establecimiento.page.ts
+++ src/app/pages/alta-establecimiento/alta-establecimiento.page.ts
@@ -702,8 +702,8 @@ export class AltaEstablecimientoPage implements OnInit {
         
         if(objeto.lista_competencias[j].ecompentencia_catalogo_competencia == 0 && objeto.lista_competencias[j].ecompentencia_foto != ''){
           objeto.lista_competencias[j].img_modificada = 1
-          objeto.lista_competencias[j].imagBase64 = this.evaluacion.lista_competencias[i].fotografia_base64
-          objeto.lista_competencias[j].ecompentencia_foto = this.fotoIdentificastePublicidad
+          objeto.lista_competencias[j].imagBase64 = this.fotoIdentificastePublicidad
+          objeto.lista_competencias[j].ecompentencia_foto = this.evaluacion.lista_competencias[i].ecompentencia_foto
           // ecompentencia_foto
           console.log("objeto edcion", this.evaluacion.lista_competencias[i].ecompentencia_foto)
           console.log("objeto a enviar", objeto.lista_competencias[j])
