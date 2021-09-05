191541feb3483522d0a8d3f9f9252a2065fd4a00
alengalvan <alengalvan95@gmail.com>
2021-09-05 15:21:47 -0500

    trabajando con one signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { MenuController, NavController } from '@ionic/angular';
+import { MenuController, NavController, Platform } from '@ionic/angular';
 import { UtilitiesService } from './services/utilities.service';
+import { OneSignal } from '@ionic-native/onesignal/ngx';
 
 @Component({
   selector: 'app-root',
@@ -19,12 +20,29 @@ export class AppComponent {
       ulr: "/cambiar-contrasenia"
     }
 
-  ]
+  ];
+  public oneSignalId: string = '25c1bedf-5abf-40cc-8edb-76a303ed229a';
+  public firebaseId: string = '438127086671'
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   constructor(private menuCtrl: MenuController,
-    private navCtrl: NavController,
-    private utilitiesService: UtilitiesService) {}
+    private navCtrl: NavController, private oneSignal: OneSignal,
+    private utilitiesService: UtilitiesService, private platform: Platform) {
+
+      platform.ready().then(() => {
+        this.oneSignal.startInit(this.oneSignalId, this.firebaseId);
+        this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+        this.oneSignal.handleNotificationReceived().subscribe((res) => {
+          console.log(res)
+        });
+
+        oneSignal.handleNotificationOpened().subscribe((res) => {
+          // do something when a notification is opened
+          console.log(res)
+        });
+        this.oneSignal.endInit();
+      })
+    }
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
   public cerrarSesion() {
