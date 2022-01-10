892fe9632ff593fe733dfec009f7b824d67210e6
alengalvan <alengalvan95@gmail.com>
2022-01-10 14:48:26 -0600

    fix

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -270,21 +270,39 @@ export class HomePage implements OnInit {
      //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      captureImage() {
       
-    let options: CameraOptions = {
-          quality: 30,
-          destinationType: this.camera.DestinationType.DATA_URL,
-          encodingType: this.camera.EncodingType.JPEG,
-          mediaType: this.camera.MediaType.PICTURE
-        }
-  
-      this.camera.getPicture(options).then((imageData) => {
+      let options2: CameraOptions = {
+        quality: 30,
+        correctOrientation: true,
+        allowEdit: false,
+        targetWidth: 400,
+        targetHeight: 600,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+     }
+
+     let galleryOptions: CameraOptions = {
+      quality: 30,
+      correctOrientation: true,
+      allowEdit: false,
+      targetWidth: 400,
+      targetHeight: 600,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+     }
+
+      console.log (JSON.stringify (galleryOptions));
+
+      this.camera.getPicture(galleryOptions).then((imageData) => {
         // imageData is either a base64 encoded string or a file URI
         // If it's base64 (DATA_URL):
         let base64Image = 'data:image/jpeg;base64,' + imageData;
         this.clickedImage = base64Image;
       }, (err) => {
         console.log(err);
-        // Handle error
+        this.utilitiesService.alert(err, err)
       });
     }
 }
